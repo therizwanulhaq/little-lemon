c30c868cfc3804cb6fec190cdfa3db6a16fd6709
rizu <therizwanulhaq@gmail.com>
2023-11-23 14:24:07 +0530

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { css } from "@emotion/css";
-import React from "react";
+import styled from "@emotion/styled";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const navElements = [
@@ -11,19 +12,52 @@ const navElements = [
   { name: "Login", path: "/login" },
 ];
 
+const HamburgerMenu = styled.span`
+  cursor: pointer;
+  position: absolute;
+  z-index: 6;
+  top: 1.1rem;
+  right: 2rem;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const DesktopNavBar = styled.ul`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileNavBar = styled.ul`
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 5;
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  padding-top: 0.3rem;
+  border-left: 1px solid #eee;
+  width: 60%;
+  height: 100%;
+  text-align: left;
+  transform: translateX(${(props) => (props.isOpen ? "0" : "100%")});
+  transition: transform 260ms;
+`;
+
 function Navbar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav>
-      <ul
-        className={css`
-          display: flex;
-          gap: 2rem;
-          align-items: center;
-          @media (max-width: 768px) {
-            display: none;
-          }
-        `}
-      >
+      <DesktopNavBar>
         {navElements.map((navLink, index) => (
           <NavLink
             to={navLink.path}
@@ -58,7 +92,6 @@ function Navbar() {
             `}
             style={({ isActive }) => {
               return {
-                // fontWeight: isActive ? "bold" : "",
                 color: isActive ? "#ffd700" : "#333333",
               };
             }}
@@ -66,7 +99,50 @@ function Navbar() {
             {navLink.name}
           </NavLink>
         ))}
-      </ul>
+      </DesktopNavBar>
+      <HamburgerMenu
+        className="material-symbols-outlined"
+        onClick={toggleMobileMenu}
+        isOpen={isMobileMenuOpen}
+        aria-label={isMobileMenuOpen ? "Close Menu" : "Open Menu"}
+      >
+        {isMobileMenuOpen ? "close" : "menu"}
+      </HamburgerMenu>
+
+      <MobileNavBar isOpen={isMobileMenuOpen}>
+        {navElements.map((navLink, index) => (
+          <NavLink
+            to={navLink.path}
+            key={index}
+            onClick={toggleMobileMenu}
+            className={css`
+              font-size: 0.9rem;
+              font-weight: 600;
+              text-transform: uppercase;
+              color: #333333;
+              padding: 1rem 2rem;
+              text-decoration: none;
+              cursor: pointer;
+              border-bottom: 1px solid #eee;
+
+              &:hover {
+                background-color: #fdfdfd;
+              }
+
+              &:first-child {
+                padding-top: 3rem; /* Adjust the top padding as needed */
+              }
+            `}
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "#ffd700" : "#333333",
+              };
+            }}
+          >
+            {navLink.name}
+          </NavLink>
+        ))}
+      </MobileNavBar>
     </nav>
   );
 }
