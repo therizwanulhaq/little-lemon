bc019e02d37d10ec0806daa2f295541128c941d0
rizu <therizwanulhaq@gmail.com>
2023-11-30 01:18:36 +0530

    adjusted nav drawer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-import logo from "../../assets/Asset 18@4x.png";
+import logo from "../../assets/Asset18@4x.png";
 
 const breakpoints = [576, 768, 992, 1200];
 
